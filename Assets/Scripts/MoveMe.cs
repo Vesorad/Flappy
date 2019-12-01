5bb8522087d5e44d94b7d2759f699e8b05c01afc
Vesorad <Vesorad@users.noreply.github.com>
2019-12-01 17:31:02 +0100

    Add topChcek and change Player

---
+++ Assets/Scripts/MoveMe.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveMe : MonoBehaviour
+{
+public float moveSpeed = 1.0f;
+public Vector3 moveVecore;
+
+
+
+
+void Start()
+ {
+  
+  
+
+ }
+ void Update()
+ {
+   transform.Translate(moveVecore * moveSpeed * Time.deltaTime);
+ }
+}
